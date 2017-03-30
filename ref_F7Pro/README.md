f9e8964cc5c5bf18e1a54b89abf3bacfab6afca7
yuwq1098 <1098654043@qq.com>
2017-03-30 14:56:57 +0800

    最初的项目搭建--项目基本结构

---
+++ ref_F7Pro/README.md
@@ -0,0 +1,2 @@
+# framework7_Geek.Yu
+学习framework7前端框架
---
+++ ref_F7Pro/fis-conf.js
@@ -0,0 +1,20 @@
+//清除其他配置，只剩下如下配置
+fis.match('*.{js,css,png}', {
+  useHash: true
+});
+
+// 清除其他配置，只保留如下配置
+fis.match('*.js', {
+  // fis-optimizer-uglify-js 插件进行压缩，已内置
+  optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+  // fis-optimizer-clean-css 插件进行压缩，已内置
+  optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+  // fis-optimizer-png-compressor 插件进行压缩，已内置
+  optimizer: fis.plugin('png-compressor')
+});
